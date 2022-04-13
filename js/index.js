a4eb7b61c303b76774b2e74f9be0e5640772ab98
Tornike Epitashvili <itorn9777@gmail.com>
2022-04-13 11:15:20 -0700

    added delete and reset list functions

--- js/index.js
+++ js/index.js
@@ -8,19 +8,35 @@ function addToDo() {
   } else {
     createListItem();
   }
-}
+};
 
 // CREATES LIST ITEM
 function createListItem() {
   let value = input.value;
-  const li = document.createElement("li");
-  // CREATES ELEMENTS FOR LIST ITEM
-  li.classList.add("list__item");
+  const deleteClass = 'list__delete';
+  const li = document.createElement('li');
+  li.classList.add('list__item');
   li.innerHTML = `
-        <p class="list__paragraph">${value}</p>
-        <button class="button list__delete">
-            <img src="images/delete.png" alt="delete-icon">
-        </button>`;
+    <p class='list__paragraph'>${value}</p>
+    <button class='button list__delete'>
+        <img src='images/delete.png' alt='delete-icon'>
+    </button>
+  `;
+  deleteClass.add
   wrapper.append(li);
+// GIVES DELETE BUTTON FUNCTION TO DELETE LIST
+  const delBtn = li.getElementsByClassName('list__delete')[0];
+  delBtn.addEventListener('click', () => {
+    deleteListItem(wrapper, li);
+  });
   input.value = "";
+};
+
+// DELETES LIST ITEM
+function deleteListItem(parent, child) {
+  parent.removeChild(child);
+};
+// CLEARS EVERY LIST ITEM FROM LIST
+function resetList() {
+  wrapper.innerHTML = '';
 }
