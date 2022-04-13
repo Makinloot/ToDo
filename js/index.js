9638f8ebf794a81dc82d92e3d0a1fac165e527b8
Tornike Epitashvili <itorn9777@gmail.com>
2022-04-13 14:50:53 -0700

    add list with enter

--- js/index.js
+++ js/index.js
@@ -2,27 +2,31 @@ const input = document.getElementById("input");
 let value = input.value;
 const wrapper = document.querySelector(".list__wrapper");
 
-document.addEventListener('DOMContentLoaded', returnLocalStorage);
+document.addEventListener("DOMContentLoaded", returnLocalStorage);
 
+input.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  if(e.keyCode == 13){
+    addToDo();
+  }
+})
 function addToDo() {
-  const error = document.getElementById('error');
+  const error = document.getElementById("error");
   let value = input.value;
   // IF INPUT VALUE IS EMPTY WILL NOT ADD LIST ITEM
   if (input.value == "") {
-    error.innerText = 'List is empty!';
+    error.innerText = "List is empty!";
   } else {
     saveToLocalStorage(value);
     createListItem(value);
-    error.innerText = '';
+    error.innerText = "";
   }
-};
-
+}
 // CREATES LIST ITEM
 function createListItem(listValue) {
   let value = input.value;
-  const li = document.createElement('li');
-  // saveToLocalStorage(value);
-  li.classList.add('list__item');
+  const li = document.createElement("li");
+  li.classList.add("list__item");
   li.innerHTML = `
     <p class='list__paragraph'>${listValue}</p>
     <button class='button list__delete'>
@@ -30,62 +34,60 @@ function createListItem(listValue) {
     </button>
   `;
   wrapper.append(li);
-// GIVES DELETE BUTTON FUNCTION TO DELETE LIST
-  const delBtn = li.getElementsByClassName('list__delete')[0];
-  delBtn.addEventListener('click', () => {
-    // removeLocalStorage(wrapper);
+  // GIVES DELETE BUTTON FUNCTION TO DELETE LIST
+  const delBtn = li.getElementsByClassName("list__delete")[0];
+  delBtn.addEventListener("click", () => {
     deleteListItem(wrapper, li);
   });
   input.value = "";
-};
+}
 
 // DELETES LIST ITEM
 function deleteListItem(parent, child) {
   removeLocalStorage(wrapper);
   parent.removeChild(child);
-};
+}
 // CLEARS EVERY LIST ITEM FROM LIST
 function resetList() {
   removeLocalStorage(wrapper);
-  wrapper.innerHTML = '';
+  wrapper.innerHTML = "";
 }
 // SAVES DATA TO LOCAL STORAGE
 function saveToLocalStorage(value) {
   let todos;
-  if(localStorage.getItem('todos') === null) {
+  if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
-  
-  todos.push(value);
-  localStorage.setItem('todos', JSON.stringify(todos));
 
+  todos.push(value);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 // DISPLAYS DATA ON SCREEN FROM LOCAL STORAGE
 function returnLocalStorage() {
   let todos;
-  if(localStorage.getItem('todos') === null) {
+  if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
 
-  todos.forEach( todo => {
+  todos.forEach((todo) => {
     createListItem(todo);
-  })
+  });
 }
 // REMOVES SAVED DATA IN LOCAL STORAGE
 function removeLocalStorage(parent) {
   let todos;
-  if(localStorage.getItem('todos') === null) {
+  if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
 
   const wrapperOfChild = parent.children[0].innerText;
   const wrapperChildren = wrapper.childNodes.length;
   todos.splice(todos.indexOf(wrapperOfChild), wrapperChildren);
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
