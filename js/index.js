9f7fa1d1ff8f2fe6dfc0e8e68d13e972216a31b5
Tornike Epitashvili <itorn9777@gmail.com>
2022-04-13 11:54:29 -0700

    added functions to save data
    saves data in local storage and on load gets data from it

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
 const input = document.getElementById("input");
+let value = input.value;
 const wrapper = document.querySelector(".list__wrapper");
 
+document.addEventListener('DOMContentLoaded', returnLocalStorage);
+
 function addToDo() {
+  let value = input.value;
   // IF INPUT VALUE IS EMPTY WILL NOT ADD LIST ITEM
   if (input.value == "") {
     console.log("red");
   } else {
-    createListItem();
+    saveToLocalStorage(value);
+    createListItem(value);
   }
 };
 
 // CREATES LIST ITEM
-function createListItem() {
+function createListItem(listValue) {
   let value = input.value;
-  const deleteClass = 'list__delete';
   const li = document.createElement('li');
+  // saveToLocalStorage(value);
   li.classList.add('list__item');
   li.innerHTML = `
-    <p class='list__paragraph'>${value}</p>
+    <p class='list__paragraph'>${listValue}</p>
     <button class='button list__delete'>
         <img src='images/delete.png' alt='delete-icon'>
     </button>
   `;
-  deleteClass.add
   wrapper.append(li);
 // GIVES DELETE BUTTON FUNCTION TO DELETE LIST
   const delBtn = li.getElementsByClassName('list__delete')[0];
@@ -39,4 +43,30 @@ function deleteListItem(parent, child) {
 // CLEARS EVERY LIST ITEM FROM LIST
 function resetList() {
   wrapper.innerHTML = '';
+}
+
+function saveToLocalStorage(value) {
+  let todos;
+  if(localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  
+  todos.push(value);
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+}
+
+function returnLocalStorage() {
+  let todos;
+  if(localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  todos.forEach( todo => {
+    createListItem(todo);
+  })
 }
